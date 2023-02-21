28072ab3e5c972b8820e49f997549d19ee5b50bf
Guilherme Lourenço <gui.lourenzo@gmail.com>
2023-02-21 03:44:15 -0300

    Module 2 tests

--- labs/02_writing_test_assertions/test_stack.py
+++ labs/02_writing_test_assertions/test_stack.py
@@ -14,16 +14,22 @@ class TestStack(TestCase):
 
     def test_push(self):
         """Test pushing an item into the stack"""
-        raise Exception("not implemented")
+        self.stack.push(9)
+        self.assertEqual(self.stack.peek(), 9)
+
 
     def test_pop(self):
         """Test popping an item of off the stack"""
-        raise Exception("not implemented")
+        self.stack.push(9)
+        self.assertEqual(self.stack.pop(), 9)
+        self.assertTrue(self.stack.is_empty())
+ 
+    # def test_peek(self):
+    #     """Test peeking at the top the stack"""
+    #     # raise Exception("not implemented")
+    #     pass
 
-    def test_peek(self):
-        """Test peeking at the top the stack"""
-        raise Exception("not implemented")
-
-    def test_is_empty(self):
-        """Test if the stack is empty"""
-        raise Exception("not implemented")
+    # def test_is_empty(self):
+    #     """Test if the stack is empty"""
+    #     # raise Exception("not implemented")
+    #     pass
