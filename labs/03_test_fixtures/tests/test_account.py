f9c5cfb4c3bb1a90d93f0e5ca1fe64b08e90c50e
Guilherme Lourenço <gui.lourenzo@gmail.com>
2023-02-21 05:05:34 -0300

    Module 2 - Fixtures tests

--- labs/03_test_fixtures/tests/test_account.py
+++ labs/03_test_fixtures/tests/test_account.py
@@ -14,18 +14,41 @@ class TestAccountModel(TestCase):
     @classmethod
     def setUpClass(cls):
         """ Connect and load data needed by tests """
+        db.create_all()
+        
+        global ACCOUNT_DATA
+        
+        with open('tests/fixtures/account_data.json') as json_data:
+            ACCOUNT_DATA = json.load(json_data)
 
     @classmethod
     def tearDownClass(cls):
         """Disconnect from database"""
+        db.session.close()
 
     def setUp(self):
         """Truncate the tables"""
+        db.session.query(Account).delete()
 
     def tearDown(self):
         """Remove the session"""
+        db.session.remove()
 
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
 
+    def test_create_an_account(self):
+        """ Test create an account """
+        account = Account(**ACCOUNT_DATA[0])
+        account.create()
+        self.assertEqual(len(Account.all()), 1)
+
+    def test_creat_all_accounts(self):
+        """ Test creating all accounts """
+
+        for data in ACCOUNT_DATA:
+            account = Account(**data)
+            account.create()
+        
+        self.assertEqual(len(Account.all()), len(ACCOUNT_DATA))
