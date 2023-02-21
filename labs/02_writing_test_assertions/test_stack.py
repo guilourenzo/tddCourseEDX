8a45e875c746ee5dc8b1dd6c9b889dc605fc0432
Guilherme Lourenço <gui.lourenzo@gmail.com>
2023-02-21 04:43:52 -0300

    Module 2 training test

--- labs/02_writing_test_assertions/test_stack.py
+++ labs/02_writing_test_assertions/test_stack.py
@@ -14,22 +14,33 @@ class TestStack(TestCase):
 
     def test_push(self):
         """Test pushing an item into the stack"""
-        self.stack.push(9)
-        self.assertEqual(self.stack.peek(), 9)
+        self.stack.push(3)
+        self.assertEqual(self.stack.peek(), 3)
 
+        self.stack.push(5)
+        self.assertEqual(self.stack.peek(), 5)
+        self.assertEqual(self.stack.pop(), 5)
 
     def test_pop(self):
         """Test popping an item of off the stack"""
-        self.stack.push(9)
-        self.assertEqual(self.stack.pop(), 9)
+        self.stack.push(3)
+        self.stack.push(5)
+        self.assertEqual(self.stack.pop(), 5)
+        self.assertEqual(self.stack.peek(), 3)
+        
+        self.stack.pop()
         self.assertTrue(self.stack.is_empty())
- 
-    # def test_peek(self):
-    #     """Test peeking at the top the stack"""
-    #     # raise Exception("not implemented")
-    #     pass
 
-    # def test_is_empty(self):
-    #     """Test if the stack is empty"""
-    #     # raise Exception("not implemented")
-    #     pass
+    def test_peek(self):
+        """Test peeking at the top the stack"""
+        self.stack.push(3)
+        self.stack.push(5)
+        self.assertEqual(self.stack.peek(), 5)
+
+    def test_is_empty(self):
+        """Test if the stack is empty"""
+        self.assertTrue(self.stack.is_empty())
+
+        self.stack.push(5)
+        self.assertFalse(self.stack.is_empty())
+    
